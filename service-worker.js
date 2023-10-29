07b66d9d99969227ed7220953da6c2f4d5f4cea2
imrokasl <99622239+imrokasl@users.noreply.github.com>
2023-10-29 14:03:18 +0200

    Experimental PWA Release

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
+  });
+
+  
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache-name')
+        .then(cache => {
+          return cache.addAll([
+            '/',
+            '/index.html',
+            '/CSS/Easteregg.css',
+            '/CSS/fullscreen.css',
+            '/CSS/normalize.css',
+            '/CSS/styles.css',
+            '/Javascript/addEventListener.js',
+            '/Javascript/mailloader.js',
+            '/Javascript/mailloader2.js',
+            // Add more URLs to cache as needed
+          ]);
+        })
+    );
+  });
+
+  
